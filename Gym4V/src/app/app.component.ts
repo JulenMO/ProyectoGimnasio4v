0802cab876bf60df6ece38f09d402b693a307ac6
julenmo <julen.medina.oses@gmail.com>
2025-01-06 01:14:08 +0100

    Avances de footer y navbar

--- Gym4V/src/app/app.component.ts
+++ Gym4V/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterNavegacionComponent } from "./footer-navegacion/footer-navegacion.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterNavegacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
